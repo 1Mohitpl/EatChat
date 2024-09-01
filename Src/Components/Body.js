3a457fd1047bf40278f20939225bf0de180ca910
Mohit kumar paul <mohit1paul@gmail.com>
2024-09-01 20:04:16 +0530

    search functionality

--- Src/Components/Body.js
+++ Src/Components/Body.js
@@ -1,18 +1,43 @@
 import { RestraurentList } from "../config";
 import RestaurantCard from "./RestraurantCard";
+import { useState } from "react";
+
+
+function filterData(searchText,restraurants) {
+
+    const result =  restraurants.filter((restaurant) => restaurant.info.name.includes(searchText));
+     return result;
+  }
 
 const Body = () => {
-    const searchText = "kfc"  // this is how created varibale in normla javascript it is hardcoded value, 
+    // const searchTxt = "kfc"  // this is how created varibale in normla javascript it is hardcoded value,
+    // seachText is a local varivable 
+    const [restraurants, setRestraurants] = useState(RestraurentList);
+    const [searchText, setSearchText] = useState(""); // to create state variable
     return (
         <>
       <div className="search-container"> 
-       <input type="text" className="search-input" placeholder="Search" value={searchText}></input>
-        <button className="search">Search</button>
+       <input type="text" className="search-input"
+        placeholder="Search" 
+         value={searchText}
+         onChange={(e)=>{
+          setSearchText(e.target.value);
+             
+         }}
+       ></input>
+        <button className="search"
+        onClick={()=>{
+            // need to filter the data form data
+            const data = filterData(searchText,restraurants);
+            setRestraurants(data);
+        }}
+        >Search</button>
+    
       </div>
 
      
        <div className="Restraulist">
-       {RestraurentList.map((Restraurent)=>{
+       {restraurants.map((Restraurent)=>{
          return <RestaurantCard {...Restraurent.info} key={Restraurent.info.id}/>
        })}
        </div>
@@ -22,3 +47,6 @@ const Body = () => {
    };
 
    export default Body;
+
+
+   
